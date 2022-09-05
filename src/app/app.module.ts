3b99537487938c706dab9a957b55a837ed855830
unknown <cogjava3111@iiht.tech>
2022-09-05 22:14:13 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoReducer } from './reducers/todo.reducer';
+import { TodoComponent } from './Components/todo/todo.component';
+import { AddTodoComponent } from './Components/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({todos:TodoReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
